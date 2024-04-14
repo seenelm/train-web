df52e17c912185685bac144d564b2cc2670fe5fa
Yassine Elmellouki <yelmellouki@gmail.com>
2024-04-14 15:29:14 -0400

    basic frontend setup

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,19 @@
 import './style.css'
-import logo from './logo4.png'
-import { setupCounter } from './counter.ts'
+import logo from './logo3.svg'
+import appstore from './appstore.svg'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
-    <a href="https://vitejs.dev" target="_blank">
+    <a target="_blank">
       <img src="${logo}" class="logo" alt="Vite logo" />
     </a>
     <h1>Train App</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
     <p class="read-the-docs">
       We're the first app that enables coaches, athletes, and parents to collaborate and communicate in real-time.
     </p>
+
+    <h3> Coming Soon to the App Store</h3>
+    <img src="${appstore}" class="appstore" alt="App Store" />
   </div>
 `
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
